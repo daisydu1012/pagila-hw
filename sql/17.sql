d3e43940867d96a00a84b84ef9ea5d04b4ba8450
daisydu1012 <daisydu1012@gmail.com>
2026-02-26 00:49:19 -0800

    save work

--- sql/17.sql
+++ sql/17.sql
@@ -4,3 +4,12 @@
  * Use tables payment, rental, customer, address, city, and country.
  * Order by country alphabetically.
  */
+
+SELECT co.country, SUM(p.amount) AS profit
+FROM country co
+JOIN city ci ON co.country_id = ci.country_id
+JOIN address a ON ci.city_id = a.city_id
+JOIN customer cu ON a.address_id = cu.address_id
+JOIN payment p ON cu.customer_id = p.customer_id
+GROUP BY co.country
+ORDER BY co.country ASC;
